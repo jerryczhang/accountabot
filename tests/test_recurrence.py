52747ab1a6c08407923d81262d72148e07fb211b
Jerry Zhang <jerrychzhang@gmail.com>
2022-12-21 16:52:44 -0800

    Add test for recurrence from_str

--- tests/test_recurrence.py
+++ tests/test_recurrence.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 
+import pytest
+
 from accountabot.data import Recurrence
 from accountabot.data import Repetition
 from accountabot.data import Weekday
@@ -27,3 +29,37 @@ def test_weekly_recurrence():
     assert r_mwf.next_occurence(datetime(2022, 12, 31)) == datetime(2023, 1, 2)
     # 2/29/2024 is a Thursday
     assert r_mwf.next_occurence(datetime(2024, 2, 29)) == datetime(2024, 3, 1)
+
+
+@pytest.mark.parametrize(
+    "value", ["random string", "daly", "weekly random", "weekly"]
+)
+def test_recurrence_from_str_invalid_value_throws(value: str):
+    with pytest.raises(ValueError):
+        Recurrence.from_str(value)
+
+
+@pytest.mark.parametrize(
+    ["value", "expected"],
+    [
+        (
+            "Daily",
+            Recurrence(Repetition.DAILY),
+        ),
+        (
+            "weekly mon, tues",
+            Recurrence(Repetition.WEEKLY, [Weekday.MONDAY, Weekday.TUESDAY]),
+        ),
+        (
+            "weekly wednesday Friday, sat",
+            Recurrence(
+                Repetition.WEEKLY,
+                [Weekday.WEDNESDAY, Weekday.FRIDAY, Weekday.SATURDAY],
+            ),
+        ),
+    ],
+)
+def test_recurrence_from_str_with_valid_values(
+    value: str, expected: Recurrence
+):
+    assert Recurrence.from_str(value) == expected
